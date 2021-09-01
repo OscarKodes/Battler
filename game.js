b2536d90bc26736369b03466525f00e7fd1e5fef
Oscar Ko <OscarKoEmail@gmail.com>
2021-09-01 15:32:06 -0400

    Add basic HP displays and a basic attack button.

--- game.js
+++ game.js
@@ -1,5 +1,22 @@
 
+// SET VARIABLES ---------------
 
-$("h1").click(function() {
-    alert("hello");
+let playerCurrentHP = 100;
+let oppoCurrentHP = 100;
+let playerDisplayHP = $(".playerDisplayHP");
+let oppoDisplayHP = $(".oppoDisplayHP");
+let atkBtn = $(".atkBtn");
+
+
+function updateHP() {
+    playerDisplayHP.text(playerCurrentHP);
+    oppoDisplayHP.text(oppoCurrentHP);
+};
+
+updateHP();
+
+atkBtn.on("click", function() {
+
+    oppoCurrentHP -= 10;
+    updateHP();
 });
