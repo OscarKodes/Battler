ff414ae886ac1b031fe6b83497d86681fef71147
Oscar Ko <OscarKoEmail@gmail.com>
2021-09-04 15:29:14 -0400

    Add knock out message for opponent when HP is 0. Hide atk btn.

--- game.js
+++ game.js
@@ -5,18 +5,60 @@ let playerCurrentHP = 100;
 let oppoCurrentHP = 100;
 let playerDisplayHP = $(".playerDisplayHP");
 let oppoDisplayHP = $(".oppoDisplayHP");
+let playerH1 = $(".player-container h1");
+let oppoH1 = $(".oppo-container h1");
+let playerContainer = $(".player-container");
+let oppoContainer = $(".oppo-container");
 let atkBtn = $(".atkBtn");
 
 
-function updateHP() {
+// FUNCTIONS ---------------------
+
+function updateDisplayHP() {
     playerDisplayHP.text(playerCurrentHP);
     oppoDisplayHP.text(oppoCurrentHP);
 };
 
-updateHP();
+function dealDamage(target, dmg) {
+
+    if (target === "oppo") {
+        oppoCurrentHP -= dmg;
+    }
+
+    else {
+        playerCurrentHP -= dmg;
+    }
+
+    checkAlive();
+}
+
+function checkAlive() {
+
+    if (oppoCurrentHP <= 0) {
+        // turn off atkbtn
+        atkBtn.hide();
+        // change text displays to knocked out.
+        oppoH1.text("Opponent has been knocked out.");
+    }
+
+    if (playerCurrentHP <= 0) {
+        // turn off atkbtn
+        atkBtn.hide();
+        // change text displays to knocked out.
+        playerH1.text("Player has been knocked out.");
+    }
+}
+
+
+// LISTENERS ---------------------
 
 atkBtn.on("click", function() {
 
-    oppoCurrentHP -= 10;
-    updateHP();
+    dealDamage("oppo", 10);
+    updateDisplayHP();
 });
+
+
+// CALLING FUNCTIONS -------------
+
+updateHP();
